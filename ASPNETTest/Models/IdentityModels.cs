7e7389dfb41ff4f932cb235cc6639f72c717b5e9
soufian17 <soufian.tichattibin@student.hu.nl>
2018-04-01 21:14:50 +0200

    commit

--- ASPNETTest/Models/IdentityModels.cs
+++ ASPNETTest/Models/IdentityModels.cs
@@ -1,10 +1,14 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Data.Entity;
+using System.IO;
 using System.Security.AccessControl;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin.Security.Provider;
+using Newtonsoft.Json.Linq;
 
 namespace ASPNETTest.Models
 {
@@ -25,8 +29,39 @@ namespace ASPNETTest.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base(@"Server=LAPTOP-IFE265TU\SQLEXPRESS;Database=ASPTest;User Id=sa; Password = SuperVeilig!;", throwIfV1Schema: false)
+            : base(@"Server=LAPTOP-IFE265TU\SQLEXPRESS;Database=ASPTest;User Id="+getUser()+"; Password = "+getPassword()+";", throwIfV1Schema: false)
         {
+        }
+
+        private static string getSetting(string prop)
+        {
+            string subfoldername = "Models";
+            string filename = "settings.json";
+            //In the Models folder add a settins.json file with within a nameless object with 2 variables username and password
+            string filepath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, subfoldername, filename);
+
+            string result = string.Empty;
+            using (StreamReader r = new StreamReader(filepath))
+            {
+                var json = r.ReadToEnd();
+                var jobj = JObject.Parse(json);
+                foreach (var item in jobj.Properties())
+                {
+                    if (item.Name == prop)
+                        result =  item.Value.ToString();
+                }
+            }
+            return result;
+        }
+
+        private static string getUser()
+        {
+            return getSetting("username");
+        }
+
+        private static string getPassword()
+        {
+            return getSetting("password");
         }
 
         public DbSet<Room> Rooms { get; set; }
